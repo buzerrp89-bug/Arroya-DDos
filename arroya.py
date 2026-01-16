913c880cc1aca8eed89d276180162ba53332501a
buzerrp89-bug <buzerrp89@gmail.com>
2026-01-16 08:42:07 +0700

    Update arroya.py

--- arroya.py
+++ arroya.py
@@ -14,11 +14,11 @@ print("""
 \033[0;30m═════════════════════════════════════════════════════════════════════\033[0m
 
   
-\033[31m      ████  ████   
+\033[31m      ████  █████   
 \033[31m     █╔═══█ █    █    
 \033[31m     █    █ █    █\033[1;32m█████   ████  █     █   ████
 \033[31m     █    █ █    █\033[1;32m█      █ █     █ █     █  █     █  
-\033[31m     ██████  ████  \033[1;32m█        █     █ █     █ █      █
+\033[31m     ██████ █████  \033[1;32m█        █     █ █     █ █      █
 \033[31m     █╔═══█ █   █ \033[1;32m█        █     █ █     █ █      █
 \033[31m     █    █ █    █\033[1;32m█         ████  ████    ████
                     \033[1;32m                        █
@@ -100,7 +100,7 @@ def initHeaders():
 def handleStatusCodes(status_code):
 	global request_counter
 	time.sleep(0.01)
-	sys.stdout.write("\n"+msg +"\033[103m\033[38;5;0m|%i \033[90mrequests|\033[0m" % request_counter)
+	sys.stdout.write(f"\033[103m\033[38;5;0m|%i \033[90mrequests|\033[0m" % request_counter)
 	print(f"\033[38;5;6m Info \033[1;36m" +str(url)+ " \033[32mpayload")
 	sys.stdout.flush()
 	if status_code == 429:
