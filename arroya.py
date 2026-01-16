0ec6420c98ff6cbae501026f8045513c84cdd7ce
buzerrp89-bug <buzerrp89@gmail.com>
2026-01-16 13:35:07 +0700

    Update arroya.py

--- arroya.py
+++ arroya.py
@@ -94,7 +94,7 @@ def initHeaders():
 def handleStatusCodes(status_code):
 	global request_counter
 	sys.stdout.write(f"\033[103m\033[38;5;0m|%i \033[90mamount of requests|\033[0m" % request_counter)
-	print("")
+	global request_counter
 	sys.stdout.write(f"\033[38;5;7m In order \033[33m" +str(url)+ " \033[1;36mpayload\033[0m")
 	sys.stdout.flush()
 	if status_code == 429:
